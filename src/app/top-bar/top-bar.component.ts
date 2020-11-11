349681970775c6ae0c93add4db97913467fe199c
Cuong Nguyen <cuongnguyenhuu97@gmail.com>
2020-11-11 21:11:20 +0700

    download png, svg

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import domtoimage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-top-bar',
@@ -12,4 +14,29 @@ export class TopBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public downloadImage(event) {
+    var imageElement = document.getElementById("main-page-image")
+    
+    domtoimage.toBlob(imageElement)
+    .then(function (blob) {
+        console.log(blob)
+        saveAs.saveAs(blob, 'sss.png');
+    });
+  }
+
+  public filter(node) {
+    return (node.tagName !== 'i');
+}
+ 
+  public saveToSVG(event){
+    domtoimage.toSvg(document.getElementById('main-page-image'), {filter: this.filter})
+    .then(function (dataUrl) {
+        /* do something */
+        var dl = document.createElement("a");
+        document.body.appendChild(dl); // This line makes it work in Firefox.
+        dl.setAttribute("href", dataUrl);
+        dl.setAttribute("download", "test.svg");
+        dl.click();
+    });
+  }
 }
